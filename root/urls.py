f975d9e89a3e32dfcc366380af18fa4316bf9f44
mono57 <aymaramono@gmail.com>
2021-03-24 09:07:09 +0100

    update runtime.txt file

--- root/urls.py
+++ root/urls.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
 
+
+class BaseHomePage(TemplateView):
+    template_name = 'index.html'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', BaseHomePage.as_view(template_name='index.html'), name='home'),
 ]
